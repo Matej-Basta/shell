7a72815f3d767002812a1e2524c5c06f3c637ef0
matej <matej.basta@email.cz>
2024-04-27 13:29:36 +0200

    updating production urls

--- next.config.mjs
+++ next.config.mjs
@@ -5,31 +5,26 @@
  */
 import NextFederationPlugin from "@module-federation/nextjs-mf";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const remote = (isServer) => {
-  /**
-   * Developer
-   */
-  // const location = isServer && false ? "ssr" : "chunks";
   const location = isServer ? "ssr" : "chunks";
   return {
-    Mf1: `Mf1@https://mf1-tawny.vercel.app/_next/static/${location}/remoteEntry.js`, // `Mf1@http://localhost:3005/_next/static/${location}/remoteEntry.js`
-    Mf2: `Mf2@https://mf2.vercel.app/remoteEntry.js`, // `Mf2@http://localhost:3008/remoteEntry.js`
+    structure: isProduction ? `structure@https://mf1-tawny.vercel.app/_next/static/${location}/remoteEntry.js` : `structure@http://localhost:3005/_next/static/${location}/remoteEntry.js`,
+    products: isProduction ? `products@https://mf-products-ten.vercel.app/_next/static/${location}/remoteEntry.js` : `products@http://localhost:3006/_next/static/${location}/remoteEntry.js`,
+    payment: isProduction? `payment@https://mf2.vercel.app/remoteEntry.js` : `payment@http://localhost:3008/remoteEntry.js`,
   };
 };
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
-    config.output.publicPath = 'https://shell-orcin.vercel.app/'; // http://localhost:3000/_next/
+    config.output.publicPath = isProduction ? 'https://shell-orcin.vercel.app/' : 'http://localhost:3000/_next/';
     config.plugins.push(
       new NextFederationPlugin({
         name: "SHELL",
         filename: "static/chunks/remoteEntry.js",
         exposes: { 
-          /**
-           * other apps component
-           * dashboard no
-           */
         },
         remotes: remote(options.isServer),
         shared: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,34 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
-//import React from "react";
-import React, {Suspense} from "react";
+import React from "react";
 import { GetServerSideProps } from 'next';
-//import RemoteMf1Component from 'Mf1/PAGES';
+import styles from "./home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 // @ts-ignore
-const RemoteMf1Component = dynamic(() => import('Mf1/PAGES'));
+const Header = dynamic(() => import('structure/Header'));
 // @ts-ignore
-//const RemoteMf1Component = React.lazy(() => import('Mf1/PAGES'));
+const Footer = dynamic(() => import('structure/Footer'));
 // @ts-ignore
-const RemoteMf2Component = dynamic(() => import('Mf2/MyComponent'), { ssr: false });
+const Products = dynamic(() => import('products/Products'));
+// @ts-ignore
+const Payment = dynamic(() => import('payment/Payment'), { ssr: false });
 
 export default function Home() {
   return (
-   <div>
-   <h1>Shell Application</h1>
-    {/* Next.js component rendered server side */}
-    {/* <Suspense fallback={<div>Loading...</div>}>
-        <RemoteMf1Component />
-    </Suspense> */}
-    <RemoteMf1Component />
-    {/* React.js component rendered client side */}
-    <RemoteMf2Component />
-   </div>
+   <>
+    <Header />
+    <main className={styles.main}>
+        <Products />
+        <Payment />
+    </main>
+    <Footer />
+   </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  // Fetch data dynamically on each request
-
   return {
     props: {
     },
