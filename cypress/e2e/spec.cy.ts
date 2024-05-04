c35af6bd07eeefc7377ddd84db31c90bcd1dd221
matej <matej.basta@email.cz>
2024-05-04 12:44:00 +0200

    adding timeouts to tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe("My First E2E Test", () => {
   it("Adding an item to the cart", () => {
     cy.visit('https://shell-orcin.vercel.app/');
-    cy.get("#5").should("contain", "Add to Cart").click();
-    cy.get("#grand_total").should("contain", "0.00 DKK");
+    cy.get("#5", {timeout: 40000}).should("contain", "Add to Cart").click();
+    cy.get("#grand_total", {timeout: 40000}).should("contain", "909.99 DKK");
   })
 })
