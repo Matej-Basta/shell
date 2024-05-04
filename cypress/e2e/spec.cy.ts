e3713819e4f4bc9ebd234a8ed7beb756047f7838
matej <matej.basta@email.cz>
2024-05-04 12:35:21 +0200

    here

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,6 +2,6 @@ describe("My First E2E Test", () => {
   it("Adding an item to the cart", () => {
     cy.visit('https://shell-orcin.vercel.app/');
     cy.get("#5").should("contain", "Add to Cart").click();
-    cy.get("#grand_total").should("contain", "909.99 DKK");
+    cy.get("#grand_total").should("contain", "0.00 DKK");
   })
 })
