1a3c65f55c3e1a86043a980ed929166b542759ba
matej <matej.basta@email.cz>
2024-05-04 13:19:08 +0200

    changing urls

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,33 @@
 import NextFederationPlugin from "@module-federation/nextjs-mf";
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isPreview = process.env.NODE_ENV === 'preview';
 
 const remote = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
-  return {
+  let structureUrl, productsUrl, paymentUrl;
+  if (isProduction) {
+    structureUrl = `structure@https://structure-dusky.vercel.app/_next/static/${location}/remoteEntry.js`; 
+    productsUrl = `products@https://mf-products-ten.vercel.app/_next/static/${location}/remoteEntry.js`; 
+    paymentUrl = `payment@https://mf2.vercel.app/remoteEntry.js`; 
+  } else if (isPreview) {
+    structureUrl = `structure@https://structure-dusky.vercel.app/_next/static/${location}/remoteEntry.js`; 
+    productsUrl = `products@https://mf-products-ten.vercel.app/_next/static/${location}/remoteEntry.js`; 
+    paymentUrl = `payment@https://mf2.vercel.app/remoteEntry.jsa`; 
+  } else {
+    structureUrl = `structure@http://localhost:3005/_next/static/${location}/remoteEntry.js`; 
+    productsUrl = `products@http://localhost:3006/_next/static/${location}/remoteEntry.js`; 
+    paymentUrl = `payment@http://localhost:3008/remoteEntry.js`;
+  }
+  /* return {
     structure: isProduction ? `structure@https://structure-dusky.vercel.app/_next/static/${location}/remoteEntry.js` : `structure@http://localhost:3005/_next/static/${location}/remoteEntry.js`,
     products: isProduction ? `products@https://mf-products-ten.vercel.app/_next/static/${location}/remoteEntry.js` : `products@http://localhost:3006/_next/static/${location}/remoteEntry.js`,
-    payment: isProduction? `payment@https://mf2.vercel.app/remoteEntry.js` : `payment@http://localhost:3008/remoteEntry.js`,
+    payment: isProduction ? `payment@https://mf2.vercel.app/remoteEntry.js` : `payment@http://localhost:3008/remoteEntry.js`,
+  }; */
+  return {
+    structure: structureUrl,
+    products: productsUrl,
+    payment: paymentUrl,
   };
 };
 const nextConfig = {
