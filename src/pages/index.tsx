f6aa14f7a197c55f4740d839daaa89157599fc74
matej <matej.basta@email.cz>
2024-05-04 14:07:36 +0200

    hello world

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <SafeComponent>
         <Payment />
       </SafeComponent>
-      {process.env.VERCEL_ENV}
+      <p> here: {process.env.VERCEL_ENV}</p>
       <p>hello world</p>
     </main>
     <Footer />
