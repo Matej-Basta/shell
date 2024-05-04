9a91f93f80a5c4bccc67f925e320269ebf0d1e9b
matej <matej.basta@email.cz>
2024-05-04 12:29:46 +0200

    having e2e workflow

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe("My First E2E Test", () => {
   it("Adding an item to the cart", () => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('https://shell-orcin.vercel.app/');
     cy.get("#5").should("contain", "Add to Cart").click();
     cy.get("#grand_total").should("contain", "909.99 DKK");
   })
